042d547c665b513c4d571e8ac6e536e534734fe2
Lutfil Khaliq <86217611+MasonHuman@users.noreply.github.com>
2022-04-01 01:15:25 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,32 @@
 let sonarPin = 0
+const buzzerPin = DigitalPin.P8
 basic.forever(function () {
     sonarPin = sonar.ping(
         DigitalPin.P2,
         DigitalPin.P3,
         PingUnit.Centimeters
     )
-    const buzzerPin = DigitalPin.P8
-    if (sonarPin >= 200) {
-        led.plot(0, 0)
+    if(sonarPin == 0){
+
         beap(buzzerPin)
-        basic.pause(2400)
-    } else if (sonarPin >= 170) {
-        beap(buzzerPin)
-        basic.pause(2200)
-    } else if (sonarPin >= 150) {
-        beap(buzzerPin)
-        basic.pause(2000)
-    } else if (sonarPin >= 130) {
-        beap(buzzerPin)
-        basic.pause(1800)
-    } else if (sonarPin >= 110) {
+        basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+        basic.pause(200)
+        basic.showLeds(`
+        # # . # #
+        # # . # #
+        # # . # #
+        # # # # #
+        # # . # #
+        `)
+        return
+    }
+    if (sonarPin >= 110) {
         beap(buzzerPin)
         basic.pause(1600)
     } else if (sonarPin >= 90) {
@@ -38,22 +45,22 @@ basic.forever(function () {
         beap(buzzerPin)
         basic.pause(500)
     } else if (sonarPin >= 15) {
-        pins.digitalWritePin(buzzerPin, 1)
+        beap(buzzerPin)
         basic.pause(250)
         pins.digitalWritePin(buzzerPin, 0)
     } else if (sonarPin >= 10) {
-        pins.digitalWritePin(buzzerPin, 1)
+        beap(buzzerPin)
         basic.pause(125)
         pins.digitalWritePin(buzzerPin, 0)
     } else if (sonarPin >= 7) {
-        pins.digitalWritePin(buzzerPin, 1)
+        beap(buzzerPin)
         basic.pause(62)
-        pins.digitalWritePin(buzzerPin, 0)
-    } else if (sonarPin < 4) {
-        pins.digitalWritePin(buzzerPin, 1)
+    } else if (sonarPin >= 4) {
+        beap(buzzerPin)
         basic.pause(31)
     }
-    led.toggle(3, 0)
+    let p = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
+    led.plotBarGraph(p, 3);
 })
 
 function beap(buzzerPin: any) {
